3d123fe5759837951ab8c463330677ce9d7b440e
NhatLinh <54463084+nhatlinhit09@users.noreply.github.com>
2023-07-04 21:06:54 +0700

    update
    update task 1

---
+++ java-turial-hocjson/json.js
@@ -0,0 +1,11 @@
+{
+     var data=[
+        {"name": "Code Learn" ,"age":2},
+        {"name": "Javascript" ,"age":25},"string any"
+
+    ]//gia tri la 1 mang
+      let count = 3000   //gia tri la 1 so
+    let more_info ={"status" :200 , "message" : "I'm fine OK"} //gia tri 1 json
+    
+}
+console.log(data.age)   
